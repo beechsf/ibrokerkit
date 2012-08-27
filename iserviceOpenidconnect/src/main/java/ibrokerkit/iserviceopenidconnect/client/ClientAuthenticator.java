e1e4187507db73c8161f3f347f5f6f6e0b662c56
peacekeeper <markus.sabadello@gmail.com>
2012-08-27 19:42:15 +0200

    remove iserviceOpenidconnect

--- iserviceOpenidconnect/src/main/java/ibrokerkit/iserviceopenidconnect/client/ClientAuthenticator.java
+++
@@ -1,9 +0,0 @@
-package pds.endpoint.oauth2.client;
-
-import net.smartam.leeloo.as.request.OAuthRequest;
-
-public interface ClientAuthenticator {
-
-	public boolean isAuthenticated(OAuthRequest oauthRequest);
-	public String getRedirectUri(OAuthRequest oauthRequest);
-}
