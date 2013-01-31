051f45f3823848deb145261cf68e7ebafe4e7b62
peacekeeper <markus.sabadello@gmail.com>
2013-01-31 07:42:46 +0100

    updating project settings, and adding ibrokerMaintenance

---
+++ ibrokerMaintenance/src/main/java/ibrokerkit/ibrokermaintenance/jobs/CheckGrsAuthorityIdJob.java
@@ -0,0 +1,23 @@
+package ibrokerkit.ibrokermaintenance.jobs;
+
+import ibrokerkit.ibrokermaintenance.IbrokerMaintenance;
+import ibrokerkit.iname4java.store.Xri;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class CheckGrsAuthorityIdJob implements Job {
+
+	private static final Log log = LogFactory.getLog(CheckGrsAuthorityIdJob.class.getName());
+
+	@Override
+	public void run() throws Exception {
+
+		for (Xri xri : IbrokerMaintenance.xriStore.listXris()) {
+
+			String grsAuthorityId = xri.getAuthorityAttribute("grs-authorityid");
+
+			if (grsAuthorityId == null) log.warn("No grs-authorityid: ");
+		}
+	}
+}
---
+++ ibrokerMaintenance/src/main/java/ibrokerkit/ibrokermaintenance/jobs/Job.java
@@ -0,0 +1,6 @@
+package ibrokerkit.ibrokermaintenance.jobs;
+
+public interface Job {
+
+	public void run() throws Exception;
+}
