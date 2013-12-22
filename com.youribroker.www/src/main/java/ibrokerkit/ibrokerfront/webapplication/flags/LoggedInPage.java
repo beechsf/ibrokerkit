d4489d2950f1f70e4ffa10d84c8b4b17b178d8bf
peacekeeper <markus.sabadello@gmail.com>
2013-12-22 10:55:55 +0100

    removing iserviceAdmin

--- com.youribroker.www/src/main/java/ibrokerkit/ibrokerfront/webapplication/flags/LoggedInPage.java
+++
@@ -1,9 +0,0 @@
-package ibrokerkit.ibrokerfront.webapplication.flags;
-
-/**
- * A tagging interface that marks pages which require the user to log in.
- * @author =peacekeeper
- */
-public interface LoggedInPage {
-
-}
--- iserviceAdmin/src/main/java/ibrokerkit/iserviceadmin/Lol.java
+++
@@ -1,5 +0,0 @@
-package ibrokerkit.iserviceadmin;
-
-public class Lol {
-
-}
